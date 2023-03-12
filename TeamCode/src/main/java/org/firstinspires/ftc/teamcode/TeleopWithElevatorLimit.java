a7a6aec17822ceab87f70a8d737426ee66f6bf68
FTC-21605 <ftc21605@gmail.com>
2023-03-12 12:52:38 -0400

    high pole power and height change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopWithElevatorLimit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopWithElevatorLimit.java
@@ -84,10 +84,10 @@ public class TeleopWithElevatorLimit extends LinearOpMode {
     //int elevatorposition_start = 0;
     static final int LOW_POLE = 870;
     static final int MEDIUM_POLE = 1300;
-    static final int HIGH_POLE = 1700;
+    static final int HIGH_POLE = 1830;
     static final double LOW_POLE_SPEED = 0.7;
     static final double MEDIUM_POLE_SPEED = 0.7;
-    static final double HIGH_POLE_SPEED = 1.;
+    static final double HIGH_POLE_SPEED = 0.8;
     static final double DOWN_SPEED = -0.5;
 
     @Override
